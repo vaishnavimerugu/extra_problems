d622ca076486326e18a6013521954c3eb1b42264
vaishnavimerugu <vaish.reddy05@gmail.com>
2015-12-10 00:02:25 +0530

    progname added

--- 2015_30_11/findMedian.c
+++ 2015_30_11/findMedian.c
@@ -12,16 +12,21 @@ void add(struct node** headref, int data)
 	current->next = *headref;
 	*headref = current;
 }
-int findMedian(struct node *head)
+float findMedian(struct node *head)
 {
-	struct node *ptr1 = head;
-	struct node *ptr2 = head;
-	while (ptr2 != NULL && ptr2->next != NULL)
-	{
-		ptr1 = ptr1->next;
-		ptr2 = ptr2->next->next;
-	}
-	return ptr1->num;
+    int count = 1;
+    struct node *mid = head,*curr=head;
+    while (curr->next!= NULL)
+    {
+        if (count%2==0)
+            mid = mid->next;
+        count++;
+        curr = curr->next;
+    }
+    if(count%2==0)
+        return ((float)((mid->next->num)+(mid->num))/2);
+    else
+        return mid->num;
 }
 void main()
 {
@@ -29,7 +34,7 @@ void main()
 	int n;
 	printf("enter the number of list elements: \n");
 	scanf("%d", &n);
-	if (n == 0)
+	if (n <= 0)
 		printf("invalid input\n");
 	else
 	{
@@ -41,8 +46,7 @@ void main()
 			scanf("%d", &data);
 			add(&head, data);
 		}
-		printf("the median of the list is : %d ", findMedian(head));
+		printf("the median of the list is : %f ", findMedian(head));
 	}
 	getch();
-
 }
